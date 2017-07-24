75fbbeb59df3407680b37fb92bc086699cd19213
Lidiya <lidiya.chernigovskaya@gmail.com>
2017-07-24 18:54:41 +0300

    Array.GetLowerBound

--- VSharp.SILI/Extern/System/SystemArray.fs
+++ VSharp.SILI/Extern/System/SystemArray.fs
@@ -8,14 +8,14 @@ open VSharp
 module SystemArray =
 
     let checkBounds state lower upper x fits k =
-            Arithmetics.simplifyGreaterOrEqual x lower (fun notTooSmall ->
-            Arithmetics.simplifyLess x upper (fun notTooLarge ->
-            Propositional.simplifyAnd notTooSmall notTooLarge (fun inBounds ->
-            Interpreter.reduceConditionalExecution state
-                (fun state k -> k (inBounds, state))
-                fits
-                (fun state k -> k (ControlFlow.throw(new IndexOutOfRangeException()), state))
-                k)))
+        Arithmetics.simplifyGreaterOrEqual x lower (fun notTooSmall ->
+        Arithmetics.simplifyLess x upper (fun notTooLarge ->
+        Propositional.simplifyAnd notTooSmall notTooLarge (fun inBounds ->
+        Interpreter.reduceConditionalExecution state
+            (fun state k -> k (inBounds, state))
+            fits
+            (fun state k -> k (ControlFlow.throw(new IndexOutOfRangeException()), state))
+            k)))
 
     let GetLength state args =
         let this, dimension = List.item 0 args, List.item 1 args in
@@ -61,3 +61,35 @@ module SystemArray =
     let get_Length state args =
         let array = Memory.deref state (List.head args) in
         (Return (Array.length array), state)
+
+    let GetLowerBound state args =
+        let this, dimension = List.item 0 args, List.item 1 args in
+        let array = Memory.deref state this in
+        let rec getLowerBound dimension = function
+            | Error _ as e -> e
+            | Array(lowerBounds, _, _, _, _) ->
+                match dimension with
+                | Error _ as e -> e
+                | Concrete(obj, _) ->
+                    let d = obj :?> int in
+                    if d < 0 || d >= lowerBounds.Length then Terms.MakeError(new IndexOutOfRangeException())
+                    else lowerBounds.[d]
+                | _ ->
+                    let lowerBound = Concrete(0, Array.lengthTermType) in
+                    let upperBound = Concrete(lowerBounds.Length, Array.lengthTermType) in
+                    checkBounds state lowerBound upperBound dimension
+                        (fun state k ->
+                            let guards =
+                                List.init lowerBounds.Length
+                                          (fun i -> simplifyEqual dimension (Concrete(i, Array.lengthTermType)) id)
+                            in
+                            let result = List.zip guards (List.ofArray lowerBounds) |> Merging.merge in
+                            k (Return result, state))
+                        (fst >> ControlFlow.resultToTerm)
+            | term -> internalfail (sprintf "expected array, but %O got!" term)
+        in
+        let result =
+            match array with
+            | Union gvs -> Merging.guardedMap (getLowerBound dimension) gvs
+            | _ -> getLowerBound dimension array
+        in (ControlFlow.throwOrReturn result, state)
