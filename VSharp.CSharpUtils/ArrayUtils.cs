2b53109ba22c33866a1d71a6d9be16a1c81b8ac4
Lidiya <lidiya.chernigovskaya@gmail.com>
2017-07-28 18:32:25 +0300

    added Array.Clear for windows

--- VSharp.CSharpUtils/ArrayUtils.cs
+++ VSharp.CSharpUtils/ArrayUtils.cs
@@ -1,18 +1,40 @@
 ﻿using System;
-using SystemArray = System.Array;
 
 namespace VSharp.CSharpUtils
 {
     public static class Array
     {
         [Implements("System.Void System.Array.ClearInternal(System.Array, System.Int32, System.Int32)")]
-        public static void ClearInternal(SystemArray array, int index, int length)
+        public static void ClearInternal(object thisNull, System.Array array, int index, int length)
         {
+            index += array.GetLowerBound(0);
             object defaultValue = Activator.CreateInstance(array.GetType().GetElementType());
             for (var ind = index; ind < length; ind++)
             {
                 array.SetValue(defaultValue, ind);
             }
+        }
+
+        [Implements("System.Void System.Array.Clear(System.Array, System.Int32, System.Int32)")]
+        public static void Clear(object thisNull, System.Array array, int index, int length)
+        {
+            if (array == null)
+                throw new ArgumentNullException("array");
+            if (length < 0)
+                throw new IndexOutOfRangeException("length < 0");
+            int lowerBound = array.GetLowerBound(0);
+            if (index < lowerBound)
+                throw new IndexOutOfRangeException("index < lower bound");
+            index -= lowerBound;
+            if (index > array.Length - length)
+                throw new IndexOutOfRangeException("index + length > size");
+            ClearInternal(thisNull, array, index, length);
+        }
+
+        [Implements("System.Int32 System.Array.GetUpperBound(this, System.Int32)")]
+        public static int GetUpperBound(System.Array array, int dimension)
+        {
+            return array.GetLowerBound(dimension) + array.GetLength(dimension) - 1;
         }
     }
 }
