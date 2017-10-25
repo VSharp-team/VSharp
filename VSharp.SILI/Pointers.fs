e8ec76ab8e28b7824482c973fda34043b3ded730
Yurii Kostyukov <kostyukov.yurii@gmail.com>
2017-10-25 18:27:00 +0300

    PointerAdditionGeneric

--- VSharp.SILI/Pointers.fs
+++ VSharp.SILI/Pointers.fs
@@ -18,6 +18,14 @@ module internal Pointers =
         | :? SymbolicPtrDiff as spd -> Some(spd.Pos, spd.Neg)
         | _ -> None
 
+    let private (|ConstantPtr|_|) (t: TermNode) =
+        match t with
+        | Constant(s, (SymbolicPtrDiffT _ as spd), tp) -> Some(s, spd :?> SymbolicPtrDiff, tp)
+        | _ -> None
+
+    let private AddPtrToSymbolicPtrDiff (p: Term) (spd: SymbolicPtrDiff) mkConst mtd k =
+        __notImplemented__()
+
     let internal locationEqual mtd addr1 addr2 =
         match TypeOf addr1, TypeOf addr2 with
         | String, String -> Strings.simplifyEquality mtd addr1 addr2
@@ -50,10 +58,64 @@ module internal Pointers =
     let internal isNull mtd ptr =
         simplifyReferenceEquality mtd ptr (MakeNullRef Null mtd) id
 
+    let rec private simplifySPDUnaryMinus mtd ischk y s k =
+        __notImplemented__()
+
+    and private simplifySPDAddition mtd l r ischk tp s k =
+        __notImplemented__()
+
+    and private simplifySPDExpressionGeneric mtd y s k repeat =
+        match term y with
+        | Expression _ -> simplifySPDExpression mtd y s (fun (y, s) -> repeat y s k)
+        | _ -> repeat y s k
+
+    and private simplifySPDExpression mtd y s k =
+        match y with
+        | Add(left, right, ischk, tp) ->
+            simplifySPDAddition mtd left right ischk tp s k
+        | UnaryMinusT(arg, ischk, tp) ->
+            simplifySPDUnaryMinus mtd ischk arg s k
+        | Sub(left, right, ischk, tp) ->
+            simplifySPDUnaryMinus mtd ischk right s (fun (right, s) ->
+            simplifySPDAddition mtd left right ischk tp s k)
+        | _ -> k (y, s)
+
+    let rec private simplifyPointerExpressionAddition mtd x y k =
+        __notImplemented__()
+
+    let rec private simplifyPointerAdditionGeneric mtd x y state k = // y must be normalized by Arithmetics!
+        let rec simplifyIndentedPointerAddition x y s k =
+            let simplifyRawPointerAddition x y s k = // x is not Indented Ref
+                let k1 = withSnd s >> k in
+                match term y with
+                | Concrete(zero, _) when CSharpUtils.Calculator.IsZero zero -> k1 x
+                | ConstantPtr(_, spd, tp) ->
+                    AddPtrToSymbolicPtrDiff x spd (fun spd -> makeSPDConst spd tp mtd) mtd k1
+                | Expression(Operator(OperationType.Add, _), _, _) ->
+                    simplifyPointerExpressionAddition mtd x y k1
+                | Expression _
+                | Concrete _
+                | Constant _ -> k1 <| IndentedRef x y mtd
+                | _ -> internalfailf "expected primitive value but got: %O" y
+
+            match term x with
+            | IndentedRef(p, shift) -> simplifyIndentedPointerAddition p (Arithmetics.add mtd shift y) s k
+            | _ -> simplifySPDExpressionGeneric mtd y s k (simplifyRawPointerAddition x)
+
+        simplifyGenericBinary "add shift to pointer" state x y k
+            (fun _ _ _ _ -> __unreachable__())
+            simplifyIndentedPointerAddition
+            (simplifyPointerAdditionGeneric mtd)
+
+    let private simplifyIndentedReferenceAddition mtd state x y k =
+        let x', y' = if Terms.IsNumeric y then x, y else y, x in
+        simplifyPointerAdditionGeneric mtd x' y' state k
+
     let internal simplifyBinaryOperation metadata op state x y k =
         match op with
-        | OperationType.Add
         | OperationType.Subtract -> __notImplemented__()
+        | OperationType.Add ->
+            simplifyIndentedReferenceAddition metadata state x y k
         | OperationType.Equal -> simplifyReferenceEquality metadata x y (withSnd state >> k)
         | OperationType.NotEqual ->
             simplifyReferenceEquality metadata x y (fun e ->
