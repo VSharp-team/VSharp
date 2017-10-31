3b350425e3741c11082e8171e6333e202f9ec958
Yurii Kostyukov <kostyukov.yurii@gmail.com>
2017-10-31 19:47:37 +0300

    [test] Pointer assignment and indirection

--- VSharp.CSharpUtils/Tests/Unsafe.cs
+++ VSharp.CSharpUtils/Tests/Unsafe.cs
@@ -4,5 +4,12 @@ namespace VSharp.CSharpUtils.Tests
 {
     public unsafe class Unsafe
     {
+        public static int ChangeThroughIndirection()
+        {
+            int x = 42;
+            int z = 14;
+            *&x = *&z;
+            return x; // 14
+        }
     }
 }
