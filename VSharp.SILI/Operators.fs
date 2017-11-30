d844b11b2583c326b3638e1dab36985109d41ce4
Misha Kosticyn <mishakosticyn@yandex.ru>
2017-11-30 17:09:04 +0300

    Added simplification of arrays equality

--- VSharp.SILI/Operators.fs
+++ VSharp.SILI/Operators.fs
@@ -1,5 +1,7 @@
 namespace VSharp
 
+open JetBrains.Decompiler.Ast
+
 [<AutoOpen>]
 module internal Operators =
 
@@ -39,3 +41,40 @@ module internal Operators =
             | Some v1, Some v2 -> simplifyAnd mtd s (simplifyEquality mtd v1.value v2.value) id
             | None, Some v2 -> s
             | _ -> __notImplemented__())
+
+    let simplifyArraysEquality mtd x y =
+
+        let inline eqTypes mtd type1 type2 k = simplifyAnd mtd (Common.is mtd type1 type2) (Common.is mtd type2 type1) k
+
+        let simplifyGInstantiatorEquality mtd gInstor1 gInstor2 =
+
+            let instorEq mtd x y k =
+                match x, y with
+                | DefaultInstantiator(_, typ1), DefaultInstantiator(_, typ2) -> eqTypes mtd typ1 typ2 k
+                | LazyInstantiator(term1, typ1), LazyInstantiator(term2, typ2)
+                | DefaultInstantiator(term1, typ1), LazyInstantiator(term2, typ2)
+                | LazyInstantiator(term1, typ1), DefaultInstantiator(term2, typ2) ->
+                    eqTypes mtd typ1 typ2 (fun equalTypes ->
+                    simplifyAnd mtd equalTypes (MakeBinary (OperationType.Equal) term1 term2 false Bool mtd) k)
+
+            List.fold (fun acc (g1, instor1) ->
+                simplifyOr mtd acc (List.fold (fun acc (g2, instor2) ->
+                    simplifyAnd mtd g1 g2 (fun guardsEq ->
+                    instorEq mtd instor1 instor2 (fun instantiatorEq ->
+                    simplifyAnd mtd acc (implies guardsEq instantiatorEq mtd) id))) (Terms.MakeTrue mtd) gInstor2) id)
+                (Terms.MakeTrue mtd)
+                gInstor1
+
+        match x.term, y.term with
+        | Array(dim1, len1, lb1, instor1, content1, l1, t1), Array(dim2, len2, lb2, instor2, content2, l2, t2) ->
+            Propositional.conjunction mtd <|
+                seq[
+                    simplifyEquality mtd dim1 dim2;
+                    simplifyEquality mtd len1 len2;
+                    simplifyHeapPointwiseEquality mtd lb1 lb2;
+                    simplifyGInstantiatorEquality mtd instor1 instor2;
+                    simplifyHeapPointwiseEquality mtd content1 content2;
+                    simplifyHeapPointwiseEquality mtd l1 l2;
+                    eqTypes mtd t1 t2 id
+                ]
+        | _ -> internalfail "not array!"
