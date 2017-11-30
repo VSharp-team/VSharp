8e07876eb71dd120c143afb51422e46b44c2ac8c
Misha Kosticyn <mishakosticyn@yandex.ru>
2017-11-30 20:01:04 +0300

    Added reference equality function

--- VSharp.SILI/Operators.fs
+++ VSharp.SILI/Operators.fs
@@ -13,6 +13,13 @@ module internal Operators =
         | Union gvs -> Merging.guardedMap refToInt gvs
         | _ -> term
 
+    let rec internal referenceEqual mtd p1 p2 =
+        let addr1 = refToInt p1 in
+        let addr2 = refToInt p2 in
+        if not(Terms.IsInteger addr1 || Terms.IsInteger addr2) then
+            internalfail "reference comparing non-reference types"
+        Arithmetics.simplifyEqual mtd addr1 addr2 id
+
     let simplifyBinaryOperation mtd op isChecked state t left right k =
         let t1 = Terms.TypeOf left in
         let t2 = Terms.TypeOf right in
