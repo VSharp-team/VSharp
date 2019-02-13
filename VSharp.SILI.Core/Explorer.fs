fc8989055146f5a260ef1fcd64e4b13ca1b9dce9
MchKosticyn <mishakosticyn@yandex.ru>
2019-02-13 15:06:05 +0300

    [feat] implemented makeSymbolicInstance for string case

--- VSharp.SILI.Core/Explorer.fs
+++ VSharp.SILI.Core/Explorer.fs
@@ -130,19 +130,19 @@ module internal Explorer =
                 let typ = frame.typ
                 let source = {id = funcId; state = state; name = {v=name}; typ = typ; location = Some location; extractor = IdTermExtractor(); typeExtractor = IdTypeExtractor()}
                 let fql = makeTopLevelFQL TopLevelStack frame.key
-                let value = Memory.makeSymbolicInstance mtd source source name fql typ
+                let value = Memory.makeSymbolicInstance mtd time source source name fql typ
                 Memory.mutateStack mtd st frame.key [] time value
             di.ContextFrames.f |> List.fold (fun state frame -> List.fold mutateLocation state frame.entries) state
         | _ -> state
 
-    let functionApplicationResult mtd (funcId : IFunctionIdentifier) name state k =
+    let functionApplicationResult mtd (funcId : IFunctionIdentifier) name state time k =
         let typ = funcId.ReturnType
         let source = {id = funcId; state = state; name = {v=name}; typ = typ; location = None; extractor = IdTermExtractor(); typeExtractor = IdTypeExtractor()}
-        Memory.makeSymbolicInstance mtd source source name None typ |> k
+        Memory.makeSymbolicInstance mtd time source source name None typ |> k
 
     let recursionApplication mtd (funcId : IFunctionIdentifier) state addr time k =
         let name = IdGenerator.startingWith <| sprintf "μ[%O]_" funcId
-        functionApplicationResult mtd funcId name state (fun res ->
+        functionApplicationResult mtd funcId name state time (fun res ->
         let recursiveResult = ControlFlow.throwOrReturn res
         let heapSymbol = RecursiveApplication(funcId, addr, time)
         let ctx : compositionContext = { mtd = mtd; addr = addr; time = time }
@@ -155,7 +155,7 @@ module internal Explorer =
         let addr = [Memory.freshAddress()]
         let time = Memory.tick()
         let name = IdGenerator.startingWith <| sprintf "λ[%O]_" funcId
-        functionApplicationResult mtd funcId name state (fun res ->
+        functionApplicationResult mtd funcId name state time (fun res ->
         let higherOrderResult = ControlFlow.throwOrReturn res
         let higherOrderState =
             { mutateStackClosure mtd funcId time state with
--- VSharp.SILI.Core/Strings.fs
+++ VSharp.SILI.Core/Strings.fs
@@ -9,18 +9,29 @@ module internal Strings =
     let strLength = "System.String.m_StringLength"
     let strArray = "System.String.m_FirstChar"
 
-    let makeString metadata time (str : string) fql =
-        let fields =
-            let stringTermLength = Concrete metadata str.Length lengthTermType
-            let arraySource = (str + "\000").ToCharArray()
-            let valMaker i = makeNumber arraySource.[i] metadata
-            let keyMaker i mtd = makeIndexArray metadata (fun _ -> makeNumber i mtd) 1
-            let arrayFQL = addToOptionFQL fql <| StructField(strArray, ArrayType(Char, Vector))
-            let lengthFQL = addToOptionFQL fql <| StructField(strLength, Arrays.lengthTermType)
-            let array = makeLinearConcreteArray metadata keyMaker valMaker (str.Length + 1) Char arrayFQL
-            Heap.ofSeq (seq [ makeKey strLength lengthFQL, { value = stringTermLength; created = time; modified = time };
-                              makeKey strArray arrayFQL, { value = array; created = time; modified = time } ])
+    let makeArrayFQL fql = addToOptionFQL fql <| StructField(strArray, ArrayType(Char, Vector))
+
+    let makeStringOfFields metadata time length array arrayFQL fql =
+        let lengthFQL = addToOptionFQL fql <| StructField(strLength, Arrays.lengthTermType)
+        let fields = Heap.ofSeq (seq [ makeKey strLength lengthFQL, { value = length; created = time; modified = time };
+                                       makeKey strArray arrayFQL, { value = array; created = time; modified = time } ])
         Struct metadata fields String
+
+    let makeConcreteStringStruct metadata time (str : string) fql =
+        let length = Concrete metadata str.Length lengthTermType
+        let arraySource = (str + "\000").ToCharArray()
+        let valMaker i = makeNumber arraySource.[i] metadata
+        let keyMaker i mtd = makeIndexArray metadata (fun _ -> makeNumber i mtd) 1
+        let arrayFQL = makeArrayFQL fql
+        let array = makeLinearConcreteArray metadata keyMaker valMaker (str.Length + 1) Char arrayFQL
+        makeStringOfFields metadata time length array arrayFQL fql
+
+    let makeStringArray metadata time length instor contents elType arrayFQL =
+        let arrLength = makeNumber 1 metadata |> add metadata length
+        let indexLength = makeIndexArray metadata (always length) 1
+        let indexLengthKey = makePathKey arrayFQL (mkArrayIndex Char) indexLength
+        let contentsWithZero = Heap.add indexLengthKey { value = makeNumber '\000' metadata; created = time; modified = time } contents
+        makeArray metadata arrLength contentsWithZero instor elType arrayFQL
 
     let simplifyStructEq mtd x y =
         match x.term, y.term with
