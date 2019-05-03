033e6176aeee4f5ec6a69751820d6d74461acea0
Yurii Kostyukov <kostyukov.yurii@gmail.com>
2019-05-03 14:17:59 +0300

    [style] Removed redundant argument

--- VSharp.SILI/SVM.fs
+++ VSharp.SILI/SVM.fs
@@ -10,8 +10,9 @@ module public SVM =
 
     let private init = lazy(Core.API.Configure (new Activator()) (new SymbolicInterpreter()))
 
-    let private prepareAndInvoke (dictionary : System.Collections.IDictionary) assemblyPath (m : MethodInfo) invoke =
+    let private prepareAndInvoke (dictionary : System.Collections.IDictionary) (m : MethodInfo) invoke =
         init.Force()
+        let assemblyPath = JetBrains.Util.FileSystemPath.Parse m.Module.FullyQualifiedName
         let qualifiedTypeName = m.DeclaringType.AssemblyQualifiedName
         let metadataMethodOption = DecompilerServices.methodInfoToMetadataMethod assemblyPath qualifiedTypeName m
         match metadataMethodOption with
@@ -23,18 +24,18 @@ module public SVM =
 //            System.Console.WriteLine("For {0}.{1} got {2}!", m.DeclaringType.Name, m.Name, ControlFlow.ResultToTerm result)
             dictionary.[m] <- summary)
 
-    let private interpretEntryPoint (dictionary : System.Collections.IDictionary) assemblyPath (m : MethodInfo) =
+    let private interpretEntryPoint (dictionary : System.Collections.IDictionary) (m : MethodInfo) =
         assert(m.IsStatic)
-        prepareAndInvoke dictionary assemblyPath m InterpretEntryPoint
+        prepareAndInvoke dictionary m InterpretEntryPoint
 
-    let private explore (dictionary : System.Collections.IDictionary) assemblyPath (m : MethodInfo) =
-        prepareAndInvoke dictionary assemblyPath m Explore
+    let private explore (dictionary : System.Collections.IDictionary) (m : MethodInfo) =
+        prepareAndInvoke dictionary m Explore
 
-    let private exploreType ignoreList ep dictionary assemblyPath (t : System.Type) =
+    let private exploreType ignoreList ep dictionary (t : System.Type) =
         let (|||) = Microsoft.FSharp.Core.Operators.(|||)
         let bindingFlags = BindingFlags.Instance ||| BindingFlags.Static ||| BindingFlags.Public ||| BindingFlags.DeclaredOnly
         if List.forall (fun keyword -> not(t.AssemblyQualifiedName.Contains(keyword))) ignoreList && t.IsPublic then
-            t.GetMethods(bindingFlags) |> FSharp.Collections.Array.iter (fun m -> if m <> ep && not m.IsAbstract then explore dictionary assemblyPath m)
+            t.GetMethods(bindingFlags) |> FSharp.Collections.Array.iter (fun m -> if m <> ep && not m.IsAbstract then explore dictionary m)
 
     let private replaceLambdaLines str =
         System.Text.RegularExpressions.Regex.Replace(str, @"@\d+(\+|\-)\d*\[Microsoft.FSharp.Core.Unit\]", "")
@@ -49,8 +50,7 @@ module public SVM =
     let public Run (assembly : Assembly) (ignoreList : List<_>) =
         let ignoreList = List.ofSeq ignoreList
         let dictionary = new Dictionary<MethodInfo, functionSummary>()
-        let path = JetBrains.Util.FileSystemPath.Parse(assembly.Location)
         let ep = assembly.EntryPoint
-        assembly.GetTypes() |> FSharp.Collections.Array.iter (exploreType ignoreList ep dictionary path)
-        if ep <> null then interpretEntryPoint dictionary path ep
+        assembly.GetTypes() |> FSharp.Collections.Array.iter (exploreType ignoreList ep dictionary)
+        if ep <> null then interpretEntryPoint dictionary ep
         System.Linq.Enumerable.ToDictionary(dictionary :> IEnumerable<_>, (fun kvp -> kvp.Key), resultToString) :> IDictionary<_, _>
