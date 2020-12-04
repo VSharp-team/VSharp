e68acd617de054aee9ab23ddfbd04245e69de788
MchKosticyn <mishakosticyn@yandex.ru>
2020-12-04 17:38:07 +0300

    [fix] object allocated inside CFA block always has less adrress then starting time of that CFA block

--- VSharp.Utils/VectorTime.fs
+++ VSharp.Utils/VectorTime.fs
@@ -6,11 +6,15 @@ module VectorTime =
     let zero = [0u]
     let infty = [System.UInt32.MaxValue]
 
-    let rec lessOrEqual (t1 : vectorTime) (t2 : vectorTime) =
-        match t1, t2 with
-        | [], _ -> true
-        | _::_, [] -> false
-        | v1::t1, v2::t2 -> v1 < v2 || (v1 = v2 && lessOrEqual t1 t2)
+    let rec compare (t1 : vectorTime) (t2 : vectorTime) =
+        List.compareWith (fun (v1 : uint32) (v2 : uint32) -> v1.CompareTo(v2)) t1 t2
+
+    let lessOrEqual (t1 : vectorTime) (t2 : vectorTime) =
+        let res = compare t1 t2
+        res = -1 || res = 0
+
+    let less (t1 : vectorTime) (t2 : vectorTime) =
+        -1 = compare t1 t2
 
     let advance (t : vectorTime) =
         List.mapLast (fun x -> if x = System.UInt32.MaxValue then internalfailf "Advancing infinite time!" else x + 1u) t
