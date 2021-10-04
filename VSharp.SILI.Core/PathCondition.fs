f0a6581f4474c03c5e39eff1414571ff4f520af3
Maksim Parshin <mxprshn@gmail.com>
2021-10-04 21:10:37 +0300

    Move independence logic to pathCondition from pathConditionIndependent

--- VSharp.SILI.Core/PathCondition.fs
+++ VSharp.SILI.Core/PathCondition.fs
@@ -1,7 +1,10 @@
 namespace VSharp.Core
 open VSharp
+open VSharp.Utils
+open VSharp.Utils.PersistentUnionFind
 
-type pathCondition = pset<term>
+type pathCondition =
+    private {constants : pUnionFind<term>; constraintsWithConstants : pdict<term, term option>}
 
 // Invariants:
 // - PC does not contain True
@@ -9,25 +12,61 @@ type pathCondition = pset<term>
 
 module internal PC =
 
-    let public empty = PersistentSet.empty<term>
-    let public isEmpty pc = PersistentSet.isEmpty pc
+    let public empty =
+        {constants = PersistentUnionFind.empty
+         constraintsWithConstants = PersistentDict.empty}
+        
+    let public isEmpty pc = PersistentDict.isEmpty pc.constraintsWithConstants
 
-    let public toSeq pc = PersistentSet.toSeq pc
+    let public toSeq pc = PersistentDict.keys pc.constraintsWithConstants
 
-    let private falsePC = PersistentSet.add empty False
+    let private falsePC =
+        {constants = PersistentUnionFind.empty
+         constraintsWithConstants = PersistentDict.add False None PersistentDict.empty}
+        
     let public isFalse pc =
-        let isFalsePC = PersistentSet.contains False pc
+        let isFalsePC = PersistentDict.contains False pc.constraintsWithConstants
         if isFalsePC then assert(toSeq pc |> Seq.length = 1)
         isFalsePC
-
+            
     let public add pc cond : pathCondition =
         match cond with
         | True -> pc
         | False -> falsePC
         | _ when isFalse pc -> falsePC
-        | _ when PersistentSet.contains !!cond pc -> falsePC
-        | _ -> PersistentSet.add pc cond
-
+        | _ when PersistentDict.contains !!cond pc.constraintsWithConstants -> falsePC
+        | _ ->
+            let condConsts = discoverConstants [cond] |> PersistentSet.ofSeq
+            let someSetElement = PersistentSet.toSeq >> Seq.tryHead
+            let pufWithNewConsts =
+                condConsts
+                |> PersistentSet.filter (fun t -> None = PersistentUnionFind.tryFind pc.constants t)
+                |> PersistentSet.fold PersistentUnionFind.add pc.constants
+            let constsWithSources =
+                Seq.map
+                    (function
+                    | ConstantT(_, src, _) as constant -> constant, src
+                    | _ -> __unreachable__()
+                    )
+            let pufMergedByConstantSource =
+                Seq.allPairs
+                    (condConsts |> PersistentSet.toSeq |> constsWithSources)
+                    (pc.constants |> PersistentUnionFind.toSeq |> constsWithSources)
+                |> Seq.filter (fun ((_, src1), (_, src2)) -> not <| src1.IndependentWith src2)
+                |> Seq.fold (fun puf ((const1, _), (const2, _)) -> PersistentUnionFind.union const1 const2 puf) pufWithNewConsts
+            let pufMergedByDependentCondition =
+                condConsts
+                |> someSetElement
+                |> function
+                    | Some(parent) ->
+                        PersistentSet.fold (fun puf t -> PersistentUnionFind.union t parent puf) pufMergedByConstantSource condConsts
+                    | None -> pufMergedByConstantSource
+            {constants = pufMergedByDependentCondition
+             constraintsWithConstants =
+                 condConsts
+                 |> someSetElement
+                 |> (fun head -> PersistentDict.add cond head pc.constraintsWithConstants)}
+            
     let public mapPC mapper (pc : pathCondition) : pathCondition =
         let mapAndAdd acc cond k =
             let acc' = mapper cond |> add acc
@@ -37,4 +76,12 @@ module internal PC =
 
     let toString pc = mapSeq toString pc |> Seq.sort |> join " /\ "
 
-    let union (pc1 : pathCondition) (pc2 : pathCondition) = Seq.fold add pc1 (toSeq pc2)
+    let public union (pc1 : pathCondition) (pc2 : pathCondition) = Seq.fold add pc1 (toSeq pc2)
+    
+(*    let public fragments pc =
+        PersistentDict.fold (fun groups cond constant ->
+            PersistentUnionFind.tryFind pc.constants constant
+            |> PersistentDict.tryFind groups
+            |> function
+                | Some(ls)
+            )*)
