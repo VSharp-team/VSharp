d2d6ceed7c03e67e0cdd0d4a5062925c08700e83
Maksim Parshin <mxprshn@gmail.com>
2021-10-08 13:59:51 +0300

    Add subset function to persistent union find

--- VSharp.Utils/PersistentUnionFind.fs
+++ VSharp.Utils/PersistentUnionFind.fs
@@ -1,6 +1,7 @@
 ﻿module VSharp.Utils.PersistentUnionFind
 
 open VSharp
+open VSharp.Utils
 
 type private node<'a> =
     | Tail of 'a
@@ -28,23 +29,36 @@ module public PersistentUnionFind =
         with
             _ -> None
             
+    let private unwrapNode = function
+        | Tail value -> value
+        | Node value -> value
+            
     let public union a b puf =
         let aParentOption = tryFind a puf
         let bParentOption = tryFind b puf
         match aParentOption, bParentOption with
         | Some(aParent), Some(bParent) when aParent <> bParent ->
-            let unwrap = function
-                | Tail value -> value
-                | Node value -> value
-            let aTail = PersistentDict.find puf.elements aParent |> unwrap
-            let bTail = PersistentDict.find puf.elements bParent |> unwrap
-            puf.elements
-            |> PersistentDict.add aParent (Tail(bTail))
-            |> PersistentDict.add bParent (Node(aTail))
-            |> (fun pdict -> {elements = pdict})
+            let aTail = PersistentDict.find puf.elements aParent |> unwrapNode
+            let bTail = PersistentDict.find puf.elements bParent |> unwrapNode
+            let mergedElements =
+                puf.elements
+                |> PersistentDict.add aParent (Tail(bTail))
+                |> PersistentDict.add bParent (Node(aTail))
+            {elements = mergedElements}
         | _ -> puf
         
-    let public add puf a =
+    let public add a puf =
         match tryFind a puf with
         | Some _ -> puf
         | None -> {elements = PersistentDict.add a (Tail(a)) puf.elements}
+        
+    let public subset a puf =
+        let rec traverse current acc =
+            let next = PersistentDict.find puf.elements current
+            let updatedDict = PersistentDict.add current next acc
+            let unwrappedNext = unwrapNode next
+            if (unwrappedNext <> a) then
+                traverse unwrappedNext updatedDict
+            else updatedDict
+        let subsetElements = traverse a PersistentDict.empty
+        {elements = subsetElements}
