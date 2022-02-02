6f3a85aa6b4c02e86cc96aeb32b5a40adcc2d584
MchKosticyn <mishakosticyn@yandex.ru>
2022-02-02 19:43:44 +0300

    [feat] Concolic supports Windows

---
+++ VSharp.ClrInteraction/communication/windowsFifoCommunicator.cpp
@@ -0,0 +1,48 @@
+#include "communication/communicator.h"
+#include "logging.h"
+#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+#include <tchar.h>
+#include <strsafe.h>
+
+using namespace icsharp;
+
+HANDLE hPipe;
+
+bool reportError() {
+    ERROR(tout << strerror(errno));
+    return false;
+}
+
+bool Communicator::open() {
+    std::string pipeEnvVar = "CONCOLIC_PIPE";
+    auto pipeFile = getenv(pipeEnvVar.c_str());
+    hPipe = CreateFile(TEXT(pipeFile), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+    if (hPipe == INVALID_HANDLE_VALUE) reportError();
+//    DWORD dwMode = PIPE_TYPE_MESSAGE;
+//    BOOL fSuccess = SetNamedPipeHandleState(hPipe, &dwMode, NULL, NULL);
+//    if (!fSuccess) reportError();
+    return true;
+}
+
+int Communicator::read(char *buffer, int count) {
+    DWORD cbRead;
+    BOOL fSuccess = ReadFile(hPipe, buffer, count, &cbRead, NULL);
+
+    if (!fSuccess || cbRead < 0) reportError();
+    return cbRead;
+}
+
+int Communicator::write(char *message, int count) {
+    DWORD cbWritten;
+    BOOL fSuccess = WriteFile(hPipe, message, count, &cbWritten, NULL);
+
+    if (!fSuccess || cbWritten < 0) reportError();
+    return cbWritten;
+}
+
+bool Communicator::close() {
+    CloseHandle(hPipe);
+    return true;
+}
