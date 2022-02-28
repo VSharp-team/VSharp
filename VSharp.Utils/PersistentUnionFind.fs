66e517c759a58a75292b02e6a59f0373aaed6ac6
mxprshn <mxprshn@gmail.com>
2022-02-28 03:18:17 +0300

    Measure union-find operations time

--- VSharp.Utils/PersistentUnionFind.fs
+++ VSharp.Utils/PersistentUnionFind.fs
@@ -31,13 +31,16 @@ module public PersistentUnionFind =
     /// </summary>
     /// <exception cref="InvalidOperationException">Element not found</exception>
     let rec public find a puf =
-        try
-            PersistentDict.find puf.elements a
-            |> function
-                | Tail _ -> a
-                | Node(next) -> find next puf
-        with
-            _ -> raise (InvalidOperationException "Element not found")
+        Stopwatch.runMeasuringTime "puf_find" (fun () ->
+            try
+                PersistentDict.find puf.elements a
+                |> function
+                    | Tail _ -> a
+                    | Node(next) -> find next puf
+            with
+                _ -> raise (InvalidOperationException "Element not found")
+        )
+
             
     /// <summary>
     /// Returns representative element of the set containing the given element or
@@ -57,43 +60,50 @@ module public PersistentUnionFind =
     /// Unions two sets containing the given elements
     /// </summary>
     let public union a b puf =
-        let aParentOption = tryFind a puf
-        let bParentOption = tryFind b puf
-        match aParentOption, bParentOption with
-        | Some(aParent), Some(bParent) when aParent <> bParent ->
-            let aTail = PersistentDict.find puf.elements aParent |> unwrapNode
-            let bTail = PersistentDict.find puf.elements bParent |> unwrapNode
-            let mergedElements =
-                puf.elements
-                |> PersistentDict.add aParent (Tail(bTail))
-                |> PersistentDict.add bParent (Node(aTail))
-            {elements = mergedElements}
-        | _ -> puf
+        Stopwatch.runMeasuringTime "puf_union" (fun () ->
+            let aParentOption = tryFind a puf
+            let bParentOption = tryFind b puf
+            match aParentOption, bParentOption with
+            | Some(aParent), Some(bParent) when aParent <> bParent ->
+                let aTail = PersistentDict.find puf.elements aParent |> unwrapNode
+                let bTail = PersistentDict.find puf.elements bParent |> unwrapNode
+                let mergedElements =
+                    puf.elements
+                    |> PersistentDict.add aParent (Tail(bTail))
+                    |> PersistentDict.add bParent (Node(aTail))
+                {elements = mergedElements}
+            | _ -> puf
+        )
+
     
     /// <summary>
     /// Adds a single-element set containing the given element. If the element already exists,
     /// does nothing
     /// </summary>
     let public add puf a =
-        match tryFind a puf with
-        | Some _ -> puf
-        | None -> {elements = PersistentDict.add a (Tail(a)) puf.elements}
+        Stopwatch.runMeasuringTime "puf_add" (fun () ->
+            match tryFind a puf with
+            | Some _ -> puf
+            | None -> {elements = PersistentDict.add a (Tail(a)) puf.elements}
+        )
         
     /// <summary>
     /// Returns a single-set union-find with the set containing the given element
     /// </summary>
     /// <exception cref="InvalidOperationException">Element not found</exception>
     let public subset a puf =
-        let rec traverse current acc =
-            let next =
-                try
-                    PersistentDict.find puf.elements current
-                with
-                    _ -> raise (InvalidOperationException "Element not found")
-            let updatedDict = PersistentDict.add current next acc
-            let unwrappedNext = unwrapNode next
-            if (unwrappedNext <> a) then
-                traverse unwrappedNext updatedDict
-            else updatedDict
-        let subsetElements = traverse a PersistentDict.empty
-        {elements = subsetElements}
+        Stopwatch.runMeasuringTime "puf_subset" (fun () ->
+            let rec traverse current acc =
+                let next =
+                    try
+                        PersistentDict.find puf.elements current
+                    with
+                        _ -> raise (InvalidOperationException "Element not found")
+                let updatedDict = PersistentDict.add current next acc
+                let unwrappedNext = unwrapNode next
+                if (unwrappedNext <> a) then
+                    traverse unwrappedNext updatedDict
+                else updatedDict
+            let subsetElements = traverse a PersistentDict.empty
+            {elements = subsetElements}
+        )
