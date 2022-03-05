a709a2526e768b16ffbdb6939b3a6734e7e80bc3
Maksim Parshin <mxprshn@gmail.com>
2022-03-05 23:24:09 +0300

    [feat] Implement all members of mutable PC

--- VSharp.SILI.Core/NewPathCondition.fs
+++ VSharp.SILI.Core/NewPathCondition.fs
@@ -9,44 +9,72 @@ module internal PC2 =
         | Tail of term * term pset
         | Node of term
         | Empty
-    
-    type PathCondition() =
-        
-        let constants = Dictionary<term, node>() 
-        let constraints = HashSet<term>()
-        let mutable isTrivialFalse = false
 
-        let rec findPrevious term =
+    let private unwrapNode =
+        function
+        | Tail(term, _) -> term
+        | Node(term) -> term
+        | Empty -> invalidOp "Cannot unwrap empty node"
+    
+    type PathCondition private(
+        constants : Dictionary<term, node>,
+        constraints : HashSet<term>,
+        isTrivialFalse : bool
+    ) =
+        let mutable isTrivialFalse = isTrivialFalse
+
+        let nextNode term =
             let mutable found = Empty
-            if constants.TryGetValue(term, &found)
-            then
-                match found with
-                | Tail(representative, constraints) -> Some(term)
-                | Node nextTerm -> findPrevious nextTerm
-                | Empty -> __unreachable__()
-            else
-                None
+            constants.TryGetValue(term, &found) |> ignore
+            found
+        
+        let rec findPrevious term =            
+            match nextNode term with
+            | Tail(_, _) -> Some(term)
+            | Node nextTerm -> findPrevious nextTerm
+            | Empty -> None
 
         let rec find term = 
-            let mutable found = Empty
-            if constants.TryGetValue(term, &found)
-            then
-                match found with
-                | Tail(representative, constraints) -> Some(representative, constraints)
-                | Node nextTerm -> find nextTerm
-                | Empty -> __unreachable__()
-            else
-                None
+            match nextNode term with
+            | Tail(representative, constraints) -> Some(representative, constraints)
+            | Node nextTerm -> find nextTerm
+            | Empty -> None
 
         let union oneConstant anotherConstant =
-            
+            match (findPrevious oneConstant), (findPrevious anotherConstant) with
+            | Some(onePrevious), Some(anotherPrevious) ->
+                match (nextNode onePrevious), (nextNode anotherPrevious) with
+                | Tail(oneRepresentative, oneConstraints), Tail(anotherRepresentative, anotherConstraints) when
+                    oneRepresentative <> anotherRepresentative ->
+                        let constraintsUnion = PersistentSet.union oneConstraints anotherConstraints
+                        constants.[onePrevious] <- Tail(anotherRepresentative, constraintsUnion)
+                        constants.[anotherPrevious] <- Node(oneRepresentative)
+                | _ -> __unreachable__()
+            | _ -> invalidOp "Constant not found in dictionary"
+
+        let subset constantInSubset =
+            seq {
+                let rec inner currentConstant =
+                    seq {
+                        if currentConstant <> constantInSubset then
+                            yield currentConstant
+                            match nextNode currentConstant with
+                            | Empty -> __unreachable__()
+                            | node -> yield! inner (unwrapNode node)
+                    }
+                match nextNode constantInSubset with
+                | Empty -> invalidOp "Constant not found in dictionary"
+                | node ->
+                    yield constantInSubset
+                    yield! inner (unwrapNode node)
+            }
 
         let becomeTrivialFalse() =
             constants.Clear()
             constraints.Clear()
             isTrivialFalse <- true
 
-        let addSubset constantsToAdd constraintsToAdd =
+        let addSubset (constants : Dictionary<term, node>) constantsToAdd constraintsToAdd =
             let firstConstant = constantsToAdd |> Seq.head
             constantsToAdd 
             |> Seq.pairwise 
@@ -58,15 +86,46 @@ module internal PC2 =
                         constants.[next] <- Node(firstConstant)
                 )
 
+        let constSourcesIndependent =
+            function
+                | ConstantT(_, oneSrc, _), ConstantT(_, anotherSrc, _) -> oneSrc.IndependentWith anotherSrc
+                | _ -> true
+
         let addNewConstraintWithMerge newConstraint = 
             let constraintConstants = discoverConstants [newConstraint]
-            let newConstants = constraintConstants |> Seq.filter (fun c -> constants.ContainsKey(c) |> not)
+            let (oldConstants, newConstants) = 
+                constraintConstants
+                    |> Seq.splitBy (fun c -> constants.ContainsKey(c))
 
-            addSubset newConstants (PersistentSet.add PersistentSet.empty newConstraint)
-            constraints.Add(newConstraint)
+            addSubset constants newConstants (PersistentSet.add PersistentSet.empty newConstraint)
+            constraints.Add(newConstraint) |> ignore
 
+            Seq.allPairs newConstants constants.Keys
+                |> Seq.filter (constSourcesIndependent >> not)
+                |> Seq.iter (fun (oneConst, anotherConst) -> union oneConst anotherConst)
+
+            match (Seq.tryHead oldConstants) with
+            | Some(someOldConstant) -> 
+                Seq.iter (fun constant -> union someOldConstant constant) oldConstants
+                match (Seq.tryHead newConstants) with
+                | Some(someNewConstant) -> union someNewConstant someOldConstant
+                | _ -> ()
+            | _ -> ()
+
+        let copy() = 
+            PathCondition(Dictionary(constants), HashSet(constraints), isTrivialFalse)
+
+        new() =
+            PathCondition(Dictionary<term, node>(), HashSet<term>(), false)
+
+        override this.ToString() =
+            Seq.map toString constraints |> Seq.sort |> join " /\ "
 
         member this.IsTrivialFalse = isTrivialFalse
+
+        member this.IsEmpty = constraints.Count = 0
+
+        member this.ToSeq() = seq constraints
 
         member this.Add newConstraint =
             match newConstraint with
@@ -74,6 +133,31 @@ module internal PC2 =
             | False -> becomeTrivialFalse()
             | _ when isTrivialFalse -> ()
             | _ when constraints.Contains(newConstraint) -> ()
+            // what if constraint is not equal to newConstraint structurally, but is equal logically?
             | _ when constraints.Contains(!!newConstraint) -> becomeTrivialFalse()
-            | _ -> addWithMerge pc cond
+            | _ -> addNewConstraintWithMerge newConstraint
+
+        member this.Map mapper =
+            let mapped = PathCondition()
+            Seq.iter (mapper >> mapped.Add) constraints
+            mapped
+
+        member this.UnionWith (anotherPc : PathCondition) =
+            let union = copy()
+            anotherPc.ToSeq() |> Seq.iter union.Add
+            union
+
+        member this.Fragments =
+            if isTrivialFalse then
+                Seq.singleton this
+            else
+                let getSubsetByRepresentative =
+                    function
+                    | Tail(representative, constraints) ->
+                        let constants = Dictionary<term, node>()
+                        addSubset constants (subset  representative) constraints
+                        let constraints = HashSet(PersistentSet.toSeq constraints)
+                        Some(PathCondition(constants, constraints, false))
+                    | _ -> None
+                Seq.choose getSubsetByRepresentative constants.Values
             
