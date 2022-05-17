40edf31bd5f5d5ce2edadc18405e3b50abfcd60f
mxprshn <mxprshn@gmail.com>
2022-05-17 16:37:17 +0300

    [fix] Make regex test harder

--- VSharp.Test/Tests/RegExTest.cs
+++ VSharp.Test/Tests/RegExTest.cs
@@ -51,9 +51,9 @@ namespace IntegrationTests
     public class RegExTest
     {
         [TestSvm(100)]
-        public static string OwnImplementationTest(char c1, char c2, char c3, char c4, char c5, char c6)
+        public static string OwnImplementationTest(char c1, char c2, char c3, char c4, char c5, char c6, char c7, char c8)
         {
-            string pattern = new string(new char[] {c1, c2, c3});
+            string pattern = new string(new char[] {c1, c2, c3, c4, c5, c6, c7, c8});
             string result = "";
             if (RegExImplementation.Match(pattern, "hello"))
             {
