0f8807df967096ffa681eb3d6a04c214e830c9cc
Dmitry Mordvinov <dvvsrd@gmail.com>
2022-10-05 14:46:24 +0300

    Two new tests for mocks.

--- VSharp.Test/Tests/Mocking.cs
+++ VSharp.Test/Tests/Mocking.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+
 namespace IntegrationTests;
 
 using VSharp.Test;
@@ -5,6 +8,16 @@ using VSharp.Test;
 public interface IDependence
 {
     int F();
+}
+
+public interface IDependence2
+{
+    int X { get; set; }
+}
+
+public interface INetwork
+{
+    int Read();
 }
 
 [TestSvmFixture]
@@ -26,4 +39,32 @@ internal class Mocking
             return 1;
         return 2;
     }
+
+    [TestSvm(100)]
+    public int ComputeWithDependence2(IDependence2 input)
+    {
+        if (input.X == 0)
+            return 0;
+
+        if (input.X > 0)
+            throw new NullReferenceException("explicit");
+        
+        input.X = -input.X;
+
+        return input.X;
+    }
+
+    [TestSvm(100)]
+    public int ReadAllFromNetwork([DisallowNull] byte[] buffer, [DisallowNull] INetwork network)
+    {
+        int next;
+        int count = 0;
+        while ((next = network.Read()) >= 0)
+        {
+            buffer[count++] = (byte)next;
+        }
+
+        return count;
+    }
+
 }
