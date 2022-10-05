01e868b8863bb9a62c7195d6cbff6e1e607d96d0
Dmitry Mordvinov <dvvsrd@gmail.com>
2022-10-05 18:29:33 +0300

    Removed F# code reference from TestExtensions

--- VSharp.TestExtensions/Allocator.cs
+++ VSharp.TestExtensions/Allocator.cs
@@ -1,7 +1,25 @@
 using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
 
 namespace VSharp.TestExtensions;
+
+internal static class CartesianProductExtension
+{
+    internal static IEnumerable<IEnumerable<T>> CartesianProduct<T>(this IEnumerable<IEnumerable<T>> sequences) 
+    { 
+        IEnumerable<IEnumerable<T>> emptyProduct = 
+            new[] { Enumerable.Empty<T>() }; 
+        return sequences.Aggregate( 
+            emptyProduct, 
+            (accumulator, sequence) => 
+                from accseq in accumulator 
+                from item in sequence 
+                select accseq.Concat(new[] {item})); 
+    }
+}
 
 public class Allocator<T>
 {
@@ -13,26 +31,27 @@ public class Allocator<T>
         _toAllocate = FormatterServices.GetUninitializedObject(ObjectType);
     }
 
-    public Allocator(object defaultValue, params int[] lengths)
+    public Allocator(object? defaultValue, params int[] lengths)
     {
         Debug.Assert(ObjectType.IsArray);
-        _toAllocate = System.Array.CreateInstance(ObjectType.GetElementType(), lengths);
-        Array.fill(_toAllocate as System.Array, defaultValue);
+        _toAllocate = Array.CreateInstance(ObjectType.GetElementType()!, lengths);
+        Fill(_toAllocate as Array, defaultValue);
     }
 
-    public Allocator(object defaultValue, int[] lengths, int[] lowerBounds)
+    public Allocator(object? defaultValue, int[] lengths, int[] lowerBounds)
     {
         Debug.Assert(ObjectType.IsArray);
-        _toAllocate = System.Array.CreateInstance(ObjectType.GetElementType(), lengths, lowerBounds);
-        Array.fill(_toAllocate as System.Array, defaultValue);
+        _toAllocate = Array.CreateInstance(ObjectType.GetElementType()!, lengths, lowerBounds);
+        Fill(_toAllocate as Array, defaultValue);
     }
 
     public object this[string fieldName]
     {
         set
         {
-            var field = ObjectType.GetField(fieldName, Reflection.allBindingFlags);
-            var property = ObjectType.GetField($"<{fieldName}>k__BackingField", Reflection.allBindingFlags);
+            var allBindingFlags = BindingFlags.IgnoreCase | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public;
+            var field = ObjectType.GetField(fieldName, allBindingFlags);
+            var property = ObjectType.GetField($"<{fieldName}>k__BackingField", allBindingFlags);
             Debug.Assert(field != null || property != null);
             field ??= property;
             if (field != null)
@@ -45,10 +64,86 @@ public class Allocator<T>
         set
         {
             Debug.Assert(ObjectType.IsArray);
-            var array = _toAllocate as System.Array;
+            var array = _toAllocate as Array;
             array?.SetValue(value, index);
         }
     }
 
     public T Object => (T) _toAllocate;
+
+    private static void FillFast<TElem>(Array? arr, TElem value)
+    {
+        if (arr != null)
+        {
+            ref var bytePtr = ref MemoryMarshal.GetArrayDataReference(arr);
+            ref var ptr = ref Unsafe.As<byte, TElem>(ref bytePtr);
+            var span = MemoryMarshal.CreateSpan(ref ptr, arr.Length);
+            span.Fill(value);
+        }
+    }
+
+    /// Fills zero-initialized array with value
+    private static void Fill(Array? arr, object? value)
+    {
+        if (value == null)
+        {
+            // Do nothing because arr is already filled with nulls
+            return;
+        }
+
+        var t = value.GetType();
+        if (arr != null && (!t.IsValueType || Nullable.GetUnderlyingType(t) != null ||
+                            value != FormatterServices.GetUninitializedObject(t)))
+        {
+            var elementType = arr.GetType().GetElementType();
+            switch (value)
+            {
+                case int i when elementType == typeof(int):
+                    FillFast(arr, i);
+                    break;
+                case byte i when elementType == typeof(byte):
+                    FillFast(arr, i);
+                    break;
+                case char i when elementType == typeof(char):
+                    FillFast(arr, i);
+                    break;
+                case byte i when elementType == typeof(uint):
+                    FillFast(arr, i);
+                    break;
+                case byte i when elementType == typeof(Int64):
+                    FillFast(arr, i);
+                    break;
+                case byte i when elementType == typeof(UInt64):
+                    FillFast(arr, i);
+                    break;
+                case byte i when elementType == typeof(double):
+                    FillFast(arr, i);
+                    break;
+                case byte i when elementType == typeof(float):
+                    FillFast(arr, i);
+                    break;
+                case byte i when elementType == typeof(Int16):
+                    FillFast(arr, i);
+                    break;
+                case byte i when elementType == typeof(UInt16):
+                    FillFast(arr, i);
+                    break;
+                case byte i when elementType == typeof(sbyte):
+                    FillFast(arr, i);
+                    break;
+                default:
+                    var rank = arr.Rank;
+                    var indices =
+                        Enumerable.Range(0, rank)
+                            .Select(i => Enumerable.Range(arr.GetLowerBound(i), arr.GetLength(i)))
+                            .CartesianProduct();
+                    foreach (var i in indices)
+                    {
+                        arr.SetValue(value, i.ToArray());
+                    }
+
+                    break;
+            }
+        }
+    }
 }
