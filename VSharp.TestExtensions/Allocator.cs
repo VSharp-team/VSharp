4aebda455b8937de9357805af00868a4b4d7b70b
MchKosticyn <mishakosticyn@yandex.ru>
2022-11-07 14:48:32 +0300

    [fix] prepared TestExtensions to copy in generated project

--- VSharp.TestExtensions/Allocator.cs
+++ VSharp.TestExtensions/Allocator.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
