217f1effca5a818661317dbe64bf3568078d444d
gsv <rsdpisuy@gmail.com>
2022-12-22 13:04:37 +0300

    Small cleanup of agent.

--- VSharp.AIAgent/agent.py
+++ VSharp.AIAgent/agent.py
@@ -1,11 +1,9 @@
 import websocket
 
-#client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#client_sock.connect(("0.0.0.0", 8080))
 ws = websocket.WebSocket()
 ws.connect("ws://0.0.0.0:8080/gameServer")
-ws.send(u'{"MessageType":"getallmaps","MessageBody":"dd"}')
+ws.send('{"MessageType":"getallmaps","MessageBody":"dd"}')
 data = ws.recv()
-ws.send(u'{"MessageType":"start","MessageBody":"{\\"MapId\\":0,\\"StepsToPlay\\":10}"}')
+ws.send('{"MessageType":"start","MessageBody":"{\\"MapId\\":0,\\"StepsToPlay\\":10}"}')
 ws.close()
 print("Received", repr(data))
