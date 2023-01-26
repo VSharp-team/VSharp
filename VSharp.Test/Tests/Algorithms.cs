88bc9a99a34caae453bc784ab8385b97616a0efc
gsv <rsdpisuy@gmail.com>
2023-01-26 13:22:50 +0300

    Bug reproduction. ShortestDistanceWeighter try to get CFG of external method

---
+++ VSharp.Test/Tests/Algorithms.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using VSharp.Test;
+
+namespace IntegrationTests;
+
+[TestSvmFixture]
+public class KMPSearch
+{
+    static List<int> Search(string pat, string txt)
+    {
+        List<int> result = new List<int>();
+        int M = pat.Length;
+        int N = txt.Length;
+ 
+        // create lps[] that will hold the longest
+        // prefix suffix values for pattern
+        int[] lps = new int[M];
+        int j = 0; // index for pat[]
+ 
+        // Preprocess the pattern (calculate lps[]
+        // array)
+        computeLPSArray(pat, M, lps);
+ 
+        int i = 0; // index for txt[]
+        while (i < N) {
+            if (pat[j] == txt[i]) {
+                j++;
+                i++;
+            }
+            if (j == M) {
+                result.Add(i - j);
+                j = lps[j - 1];
+            }
+ 
+            // mismatch after j matches
+            else if (i < N && pat[j] != txt[i]) {
+                // Do not match lps[0..lps[j-1]] characters,
+                // they will match anyway
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i = i + 1;
+            }
+        }
+
+        return result;
+    }
+ 
+    static void computeLPSArray(string pat, int M, int[] lps)
+    {
+        // length of the previous longest prefix suffix
+        int len = 0;
+        int i = 1;
+        lps[0] = 0; // lps[0] is always 0
+ 
+        // the loop calculates lps[i] for i = 1 to M-1
+        while (i < M) {
+            if (pat[i] == pat[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else // (pat[i] != pat[len])
+            {
+                // This is tricky. Consider the example.
+                // AAACAAAA and i = 7. The idea is similar
+                // to search step.
+                if (len != 0) {
+                    len = lps[len - 1];
+ 
+                    // Also, note that we do not increment
+                    // i here
+                }
+                else // if (len == 0)
+                {
+                    lps[i] = len;
+                    i++;
+                }
+            }
+        }
+    }
+    
+    [TestSvm(100)]
+    public static List<int> KMPSearchMain(string txt, string pattern)
+    {
+        var result = Search(pattern, txt);
+        return result;
+    }
+}
