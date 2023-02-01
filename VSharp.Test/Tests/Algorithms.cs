c48fdc732e945fb3fde42657ef01811243940a29
gsv <rsdpisuy@gmail.com>
2023-02-01 16:38:48 +0300

    New test: tests generation too long.

--- VSharp.Test/Tests/Algorithms.cs
+++ VSharp.Test/Tests/Algorithms.cs
@@ -220,3 +220,111 @@ public class KruskalGraph
         return new Tuple<int, List<Tuple<int, int, int>>>(minimumCost, resultEdges);
     }
 }
+
+[TestSvmFixture]
+public static class ConvexHull {
+    static HashSet<List<int> > hull
+        = new HashSet<List<int> >();
+    // Stores the result (points of convex hull)
+ 
+    // Returns the side of point p with respect to line
+    // joining points p1 and p2.
+    public static int findSide(List<int> p1, List<int> p2,
+                               List<int> p)
+    {
+        int val = (p[1] - p1[1]) * (p2[0] - p1[0])
+                  - (p2[1] - p1[1]) * (p[0] - p1[0]);
+ 
+        if (val > 0) {
+            return 1;
+        }
+        if (val < 0) {
+            return -1;
+        }
+        return 0;
+    }
+ 
+    // returns a value proportional to the distance
+    // between the point p and the line joining the
+    // points p1 and p2
+    public static int lineDist(List<int> p1, List<int> p2,
+                               List<int> p)
+    {
+        return Math.Abs((p[1] - p1[1]) * (p2[0] - p1[0])
+                        - (p2[1] - p1[1]) * (p[0] - p1[0]));
+    }
+ 
+    // End points of line L are p1 and p2. side can have
+    // value 1 or -1 specifying each of the parts made by
+    // the line L
+    public static void quickHull(List<List<int> > a, int n,
+                                 List<int> p1, List<int> p2,
+                                 int side)
+    {
+        int ind = -1;
+        int max_dist = 0;
+ 
+        // finding the point with maximum distance
+        // from L and also on the specified side of L.
+        for (int i = 0; i < n; i++) {
+            int temp = lineDist(p1, p2, a[i]);
+            if (findSide(p1, p2, a[i]) == side
+                && temp > max_dist) {
+                ind = i;
+                max_dist = temp;
+            }
+        }
+ 
+        // If no point is found, add the end points
+        // of L to the convex hull.
+        if (ind == -1) {
+            hull.Add(p1);
+            hull.Add(p2);
+            return;
+        }
+ 
+        // Recur for the two parts divided by a[ind]
+        quickHull(a, n, a[ind], p1,
+                  -findSide(a[ind], p1, p2));
+        quickHull(a, n, a[ind], p2,
+                  -findSide(a[ind], p2, p1));
+    }
+ 
+    public static void computeHull(List<List<int> > a, int n)
+    {
+        // a[i].second -> y-coordinate of the ith point
+        if (n < 3)
+        {
+            throw new Exception("Convex hull not possible for given set of points.");
+        }
+ 
+        // Finding the point with minimum and
+        // maximum x-coordinate
+        int min_x = 0;
+        int max_x = 0;
+        for (int i = 1; i < n; i++) {
+            if (a[i][0] < a[min_x][0]) {
+                min_x = i;
+            }
+            if (a[i][0] > a[max_x][0]) {
+                max_x = i;
+            }
+        }
+ 
+        // Recursively find convex hull points on
+        // one side of line joining a[min_x] and
+        // a[max_x]
+        quickHull(a, n, a[min_x], a[max_x], 1);
+        quickHull(a, n, a[min_x], a[max_x], -1);
+        
+    }
+ 
+    // Driver code
+    [TestSvm(100)]
+    public static HashSet<List<int>> ConvexHullMain(List<List<int> > points)
+    {
+        int n = points.Count;
+        computeHull(points, n);
+        return hull;
+    }
+}
