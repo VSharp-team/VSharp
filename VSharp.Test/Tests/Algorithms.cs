1854d7ffae3b979bc597ac096097dd99e99748eb
gsv <rsdpisuy@gmail.com>
2023-02-09 10:54:49 +0300

    New test: Knapsack.

--- VSharp.Test/Tests/Algorithms.cs
+++ VSharp.Test/Tests/Algorithms.cs
@@ -762,3 +762,137 @@ public static bool EulerMain(EulerGraph g)
 	return g.isEulerianCycle();
 }
 }
+
+[TestSvmFixture]
+	class KnapsackBag : IEnumerable<KnapsackBag.Item>
+	{
+		List<Item> items;
+		const int MaxWeightAllowed = 400;
+
+		public KnapsackBag()
+		{
+			items = new List<Item>();
+		}
+
+		void AddItem(Item i)
+		{
+			if ((TotalWeight + i.Weight) <= MaxWeightAllowed)
+				items.Add(i);
+		}
+
+		[TestSvm(100)]
+		public void Calculate(List<Item> items)
+		{
+			foreach (Item i in Sorte(items))
+			{
+				AddItem(i);
+			}
+		}
+
+		[TestSvm(100)]
+		public List<Item> Sorte(List<Item> inputItems)
+		{
+			List<Item> choosenItems = new List<Item>();
+			for (int i = 0; i < inputItems.Count; i++)
+			{
+				int j = -1;
+				if (i == 0)
+				{
+					choosenItems.Add(inputItems[i]);
+				}
+
+				if (i > 0)
+				{
+					if (!RecursiveF(inputItems, choosenItems, i, choosenItems.Count - 1, false, ref j))
+					{
+						choosenItems.Add(inputItems[i]);
+					}
+				}
+			}
+
+			return choosenItems;
+		}
+
+		bool RecursiveF(List<Item> knapsackItems, List<Item> choosenItems, int i, int lastBound, bool dec,
+			ref int indxToAdd)
+		{
+			if (!(lastBound < 0))
+			{
+				if (knapsackItems[i].ResultWV < choosenItems[lastBound].ResultWV)
+				{
+					indxToAdd = lastBound;
+				}
+
+				return RecursiveF(knapsackItems, choosenItems, i, lastBound - 1, true, ref indxToAdd);
+			}
+
+			if (indxToAdd > -1)
+			{
+				choosenItems.Insert(indxToAdd, knapsackItems[i]);
+				return true;
+			}
+
+			return false;
+		}
+
+		#region IEnumerable<Item> Members
+
+		IEnumerator<Item> IEnumerable<Item>.GetEnumerator()
+		{
+			foreach (Item i in items)
+				yield return i;
+		}
+
+		#endregion
+
+		#region IEnumerable Members
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		{
+			return items.GetEnumerator();
+		}
+
+		#endregion
+
+		public int TotalWeight
+		{
+			get
+			{
+				var sum = 0;
+				foreach (Item i in this)
+				{
+					sum += i.Weight;
+				}
+
+				return sum;
+			}
+		}
+
+		public class Item
+		{
+			public string Name { get; set; }
+			public int Weight { get; set; }
+			public int Value { get; set; }
+
+			public int ResultWV
+			{
+				get { return Weight - Value; }
+			}
+
+			public override string ToString()
+			{
+				return "Name : " + Name + "        Wieght : " + Weight + "       Value : " + Value +
+				       "     ResultWV : " + ResultWV;
+			}
+		}
+	}
+
+	class Knapsack
+	{
+		static KnapsackBag KnapsackMain(List<KnapsackBag.Item> knapsackItems)
+		{
+			KnapsackBag b = new KnapsackBag();
+			b.Calculate(knapsackItems);
+			return b;
+		}
+	}
