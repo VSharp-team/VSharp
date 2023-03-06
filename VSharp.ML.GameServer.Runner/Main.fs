f1b474c7735077ad73639a2043916497e3d358e5
gsv <rsdpisuy@gmail.com>
2023-03-06 09:52:47 +0300

    List of maps to separated file. Turn some chess-related maps off.

--- VSharp.ML.GameServer.Runner/Main.fs
+++ VSharp.ML.GameServer.Runner/Main.fs
@@ -1,5 +1,4 @@
 open System.IO
-open System.Text.Json
 open Argu
 open Microsoft.FSharp.Core
 open Suave
@@ -13,9 +12,9 @@ open VSharp
 open VSharp.Core
 open VSharp.Interpreter.IL
 open VSharp.ML.GameServer.Messages
+open VSharp.ML.GameServer.Maps
 open VSharp.Runner
-  
-  
+   
 type CliArguments =
     | [<Unique>] Port of int
     interface IArgParserTemplate with
