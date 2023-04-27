ab674d0fb9c2d2d9b1580cd9e579bcd61beb3895
Max Nigmatulin <mvnigma@gmail.com>
2023-04-27 18:29:28 +0300

    Add weight dump after every gen

--- VSharp.ML.AIAgent/r_learn.py
+++ VSharp.ML.AIAgent/r_learn.py
@@ -149,8 +149,14 @@ def r_learn(
     new_gen_provider_function: NewGenProviderFunction,
     connection_manager: ConnectionManager,
 ) -> None:
-    def _is_last_epoch(epoch):
+    def is_last_epoch(epoch):
         return epoch == epochs - 1
+
+    def dump_survived(models):
+        survived = ""
+        for model in models:
+            survived += str(model) + "\n"
+        append_to_tables_file(f"survived models: {survived}")
 
     for epoch in range(epochs):
         epoch_string = f"Epoch {epoch + 1}/{epochs}"
@@ -181,11 +187,7 @@ def r_learn(
             + "\n"
         )
 
-        if not _is_last_epoch(epoch):
+        dump_survived(models)
+        if not is_last_epoch(epoch):
             models = new_gen_provider_function(train_game_maps_model_results)
         invalidate_cache(models)
-
-    survived = ""
-    for model in models:
-        survived += str(model) + "\n"
-    append_to_tables_file(f"survived models: {survived}")
