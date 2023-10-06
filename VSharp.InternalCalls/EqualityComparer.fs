44b735f76e231174384a7d0e7e33a3eff116284d
MchKosticyn <mishakosticyn@yandex.ru>
2023-10-06 17:34:20 +0300

    [fix] fixed 'Equals' internal calls
    - fixed structural equality

--- VSharp.InternalCalls/EqualityComparer.fs
+++ VSharp.InternalCalls/EqualityComparer.fs
@@ -35,16 +35,34 @@ module EqualityComparer =
         let typ = Helpers.unwrapType wrappedType
         createEqualityComparer state typ
 
-    let internal structuralEquality (state : state) block1 block2 =
-        let checkContents () =
-            let blockType = Terms.MostConcreteTypeOfRef state block1
+    let rec internal structuralEquality (state : state) term1 term2 : term =
+        let checkFields blockType =
             let compareOneField acc (field, _) =
-                let block1Field = Memory.ReadField state block1 field
-                let block2Field = Memory.ReadField state block2 field
-                (block1Field === block2Field) &&& acc
+                let block1Field = Memory.ReadField state term1 field
+                let block2Field = Memory.ReadField state term2 field
+                if acc <> False() then
+                    acc &&& structuralEquality state block1Field block2Field
+                else acc
             let blockFields = Reflection.fieldsOf false blockType
             Array.fold compareOneField (True()) blockFields
-        let typeEquals = Types.RefIsRef state block1 block2 &&& Types.RefIsRef state block2 block1
-        if typeEquals = True() then checkContents ()
-        elif typeEquals = False() then False()
-        else __insufficientInformation__ "unable to check structural equality: %O, %O" block1 block2
+        let isRef1 = IsRefOrPtr term1
+        let isRef2 = IsRefOrPtr term2
+        if isRef1 || isRef2 then
+            if isRef1 && isRef2 then
+                let typeEquals = Types.RefIsRef state term1 term2 &&& Types.RefIsRef state term2 term1
+                if typeEquals = True() then
+                    let blockType = Terms.MostConcreteTypeOfRef state term1
+                    if TypeUtils.isPrimitive blockType || blockType.IsEnum then
+                        let value1 = Memory.Read state term1
+                        let value2 = Memory.Read state term2
+                        structuralEquality state value1 value2
+                    else checkFields blockType
+                elif typeEquals = False() then False()
+                else __insufficientInformation__ $"unable to check structural equality: {term1}, {term2}"
+            else False()
+        else
+            let t1 = TypeOf term1
+            let t2 = TypeOf term2
+            if (TypeUtils.isPrimitive t1 || t1.IsEnum) && (TypeUtils.isPrimitive t2 || t2.IsEnum) then
+                term1 === term2
+            else __notImplemented__()
