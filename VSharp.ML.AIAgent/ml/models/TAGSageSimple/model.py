d4f9bad96bac5c3607b5f2d45cf38e2dac0964b1
katyacyfra <katyacyfra@gmail.com>
2023-11-08 20:55:47 +0300

    fix codestyle

---
+++ VSharp.ML.AIAgent/ml/models/TAGSageSimple/model.py
@@ -0,0 +1,47 @@
+import torch
+from torch.nn import Linear
+from torch_geometric.nn import TAGConv, GraphConv, SAGEConv
+
+
+class StateModelEncoder(torch.nn.Module):
+    def __init__(self, hidden_channels, out_channels):
+        super().__init__()
+        self.conv1 = TAGConv(5, hidden_channels, 2)
+        self.conv2 = TAGConv(hidden_channels, hidden_channels, 3)  # TAGConv
+        self.conv3 = GraphConv((-1, -1), hidden_channels)  # SAGEConv
+        self.conv4 = SAGEConv((-1, -1), hidden_channels)
+        self.lin = Linear(hidden_channels, out_channels)
+
+    def forward(
+        self,
+        game_x,
+        state_x,
+        edge_index_v_v,
+        edge_type_v_v,
+        edge_index_history_v_s,
+        edge_attr_history_v_s,
+        edge_index_in_v_s,
+        edge_index_s_s,
+    ):
+        game_x = self.conv1(
+            game_x,
+            edge_index_v_v,
+        ).relu()
+
+        state_x = self.conv3(
+            (game_x, state_x),
+            edge_index_history_v_s,
+            edge_attr_history_v_s,
+        ).relu()
+
+        state_x = self.conv4(
+            (game_x, state_x),
+            edge_index_in_v_s,
+        ).relu()
+
+        state_x = self.conv2(
+            state_x,
+            edge_index_s_s,
+        ).relu()
+
+        return self.lin(state_x)
