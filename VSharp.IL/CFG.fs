90dc99f099ced0d1bfa29f8e1e936ee5f71b282d
gsv <rsdpisuy@gmail.com>
2023-12-29 17:37:58 +0300

    Remove shortcut edge only if callFrom <> returnTo

--- VSharp.IL/CFG.fs
+++ VSharp.IL/CFG.fs
@@ -577,8 +577,9 @@ type ApplicationGraph(getNextBasicBlockGlobalId,applicationGraphDelta:Applicatio
             
             let added = callTo.IncomingCallEdges.Add callFrom
             assert added
-            let removed = callFrom.OutgoingEdges.Remove CfgInfo.TerminalForCFGEdge //CallGraph.dummyTerminalForCallShortcut
-            assert removed
+            if callFrom <> returnTo then
+                let removed = callFrom.OutgoingEdges.Remove CfgInfo.TerminalForCFGEdge //CallGraph.dummyTerminalForCallShortcut
+                assert removed
         else ()
 
     let moveState (initialPosition: codeLocation) (stateWithNewPosition: IGraphTrackableState) =
